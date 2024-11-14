8143a09e39e6d5e50a3bb7e39bdee34e317e60a5
Joe Caulfield <joe.caulfield@anza.xyz>
2024-11-14 20:22:35 +0800

    make publish-rust script work for program

--- scripts/publish-rust.mjs
+++ scripts/publish-rust.mjs
@@ -1,15 +1,55 @@
 #!/usr/bin/env zx
 import 'zx/globals';
-import { cliArguments, getCargo, workingDirectory } from './utils.mjs';
+import { getCargo, workingDirectory } from './utils.mjs';
 
+class Publisher {
+  libType;
+
+  constructor() {
+    const lib = process.argv[3];
+    if (!lib) {
+      throw new Error('A library type must be provided.');
+    }
+    if (lib !== 'client' && lib !== 'program') {
+      throw new Error('Invalid library type. Allowed values are "client" or "program".');
+    }
+    this.libType = lib;
+  }
+
+  path() {
+    if (this.libType === 'client') {
+      return 'clients/rust';
+    } else {
+      return 'program';
+    }
+  }
+
+  message() {
+    if (this.libType === 'client') {
+      return 'Rust client';
+    } else {
+      return 'Program';
+    }
+  }
+
+  tagPrefix() {
+    if (this.libType === 'client') {
+      return 'rust';
+    } else {
+      return 'program';
+    }
+  }
+}
+
+const publisher = new Publisher();
 const dryRun = argv['dry-run'] ?? false;
-const [level] = cliArguments();
+const [level] = process.argv.slice(4);
 if (!level) {
   throw new Error('A version level — e.g. "path" — must be provided.');
 }
 
-// Go to the client directory and install the dependencies.
-cd(path.join(workingDirectory, 'clients', 'rust'));
+// Go to the directory and install the dependencies.
+cd(path.join(workingDirectory, publisher.path()));
 
 // Publish the new version.
 const releaseArgs = dryRun
@@ -23,7 +63,7 @@ if (dryRun) {
 }
 
 // Get the new version.
-const newVersion = getCargo(path.join('clients', 'rust')).package.version;
+const newVersion = getCargo(publisher.path()).package.version;
 
 // Expose the new version to CI if needed.
 if (process.env.CI) {
@@ -34,7 +74,7 @@ if (process.env.CI) {
 await $`git reset --soft HEAD~1`;
 
 // Commit the new version.
-await $`git commit -am "Publish Rust client v${newVersion}"`;
+await $`git commit -am "Publish ${publisher.message()} v${newVersion}"`;
 
 // Tag the new version.
-await $`git tag -a rust@v${newVersion} -m "Rust client v${newVersion}"`;
+await $`git tag -a ${publisher.tagPrefix()}@v${newVersion} -m "${publisher.message()} v${newVersion}"`;
