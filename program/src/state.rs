23e02a3d773bd34dca125fcbe316cd1d796ee28d
Kevin Heavey <heaveykevin2@gmail.com>
2025-04-03 10:59:56 +0100

    use solana-config-interface in solana-config-program

--- program/src/state.rs
+++ program/src/state.rs
@@ -1,15 +1,3 @@
 //! Program state types.
 
-use {
-    serde::{Deserialize, Serialize},
-    solana_program::{pubkey::Pubkey, short_vec},
-};
-
-/// A collection of keys to be stored in Config account data.
-#[derive(Debug, Default, Deserialize, Serialize)]
-pub struct ConfigKeys {
-    /// Each key tuple comprises a unique `Pubkey` identifier,
-    /// and `bool` whether that key is a signer of the data.
-    #[serde(with = "short_vec")]
-    pub keys: Vec<(Pubkey, bool)>,
-}
+pub use solana_config_interface::ConfigKeys;
